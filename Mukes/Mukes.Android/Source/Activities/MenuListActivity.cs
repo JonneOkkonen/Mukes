b92f9d976814952dd1af4ad90b9671e0c08984e3
Jonne Okkonen <okkonen.jonne@gmail.com>
2018-03-26 15:45:06 +0300

    Create Basic App Layout
    Create Basic Layout
    Add Test Data to Layout
    Delete unnecessary start files
    Edit Folder Structure

---
+++ Mukes/Mukes.Android/Source/Activities/MenuListActivity.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+using Android.App;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using System.Collections.Generic;
+
+namespace Mukes.Droid
+{
+    [Activity(Label = "Mukes", Icon = "@drawable/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MenuListActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Content View
+            SetContentView(Resource.Layout.MenuListView);
+
+            // Elements
+            var heading = FindViewById<TextView>(Resource.Id.heading);
+            var menuList = FindViewById<ListView>(Resource.Id.menuList);
+
+            // MenuList
+            List<string> menu = new List<string>();
+            
+            // Add Data
+            heading.Text = "Ruokalista";
+            menu.Add("Maanantai");
+            menu.Add("Tiistai");
+
+            // MenuAdapter
+            ArrayAdapter<string> menuAdapter = new ArrayAdapter<string>(this, Android.Resource.Layout.SimpleListItem1, menu);
+
+            // Add Adapter to menuList
+            menuList.Adapter = menuAdapter;
+
+        }
+    }
+}
+
