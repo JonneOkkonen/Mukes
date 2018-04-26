a9da1e0b36296d8c5aa71c46c9ec3fb993c17622
Jonne Okkonen <okkonen.jonne@gmail.com>
2018-04-26 13:49:45 +0300

    Add Splash Screen

--- Mukes/Mukes.Android/Resources/Resource.designer.cs
+++ Mukes/Mukes.Android/Resources/Resource.designer.cs
@@ -2192,26 +2192,26 @@ namespace Mukes.Droid
 			// aapt resource value: 0x7f020053
 			public const int avd_hide_password = 2130837587;
 			
-			// aapt resource value: 0x7f020116
-			public const int avd_hide_password_1 = 2130837782;
-			
-			// aapt resource value: 0x7f020117
-			public const int avd_hide_password_2 = 2130837783;
-			
 			// aapt resource value: 0x7f020118
-			public const int avd_hide_password_3 = 2130837784;
+			public const int avd_hide_password_1 = 2130837784;
+			
+			// aapt resource value: 0x7f020119
+			public const int avd_hide_password_2 = 2130837785;
+			
+			// aapt resource value: 0x7f02011a
+			public const int avd_hide_password_3 = 2130837786;
 			
 			// aapt resource value: 0x7f020054
 			public const int avd_show_password = 2130837588;
 			
-			// aapt resource value: 0x7f020119
-			public const int avd_show_password_1 = 2130837785;
-			
-			// aapt resource value: 0x7f02011a
-			public const int avd_show_password_2 = 2130837786;
-			
 			// aapt resource value: 0x7f02011b
-			public const int avd_show_password_3 = 2130837787;
+			public const int avd_show_password_1 = 2130837787;
+			
+			// aapt resource value: 0x7f02011c
+			public const int avd_show_password_2 = 2130837788;
+			
+			// aapt resource value: 0x7f02011d
+			public const int avd_show_password_3 = 2130837789;
 			
 			// aapt resource value: 0x7f020055
 			public const int design_bottom_navigation_item_background = 2130837589;
@@ -2768,11 +2768,11 @@ namespace Mukes.Droid
 			// aapt resource value: 0x7f02010d
 			public const int notification_icon_background = 2130837773;
 			
-			// aapt resource value: 0x7f020114
-			public const int notification_template_icon_bg = 2130837780;
+			// aapt resource value: 0x7f020116
+			public const int notification_template_icon_bg = 2130837782;
 			
-			// aapt resource value: 0x7f020115
-			public const int notification_template_icon_low_bg = 2130837781;
+			// aapt resource value: 0x7f020117
+			public const int notification_template_icon_low_bg = 2130837783;
 			
 			// aapt resource value: 0x7f02010e
 			public const int notification_tile_bg = 2130837774;
@@ -2781,16 +2781,22 @@ namespace Mukes.Droid
 			public const int notify_panel_notification_icon_bg = 2130837775;
 			
 			// aapt resource value: 0x7f020110
-			public const int sweden_flag = 2130837776;
+			public const int splash_screen = 2130837776;
 			
 			// aapt resource value: 0x7f020111
-			public const int sweden_flag_selected = 2130837777;
+			public const int SplashScreen = 2130837777;
 			
 			// aapt resource value: 0x7f020112
-			public const int united_kingdom_flag = 2130837778;
+			public const int sweden_flag = 2130837778;
 			
 			// aapt resource value: 0x7f020113
-			public const int united_kingdom_flag_selected = 2130837779;
+			public const int sweden_flag_selected = 2130837779;
+			
+			// aapt resource value: 0x7f020114
+			public const int united_kingdom_flag = 2130837780;
+			
+			// aapt resource value: 0x7f020115
+			public const int united_kingdom_flag_selected = 2130837781;
 			
 			static Drawable()
 			{
@@ -4555,6 +4561,9 @@ namespace Mukes.Droid
 			
 			// aapt resource value: 0x7f0b004d
 			public const int RtlUnderlay_Widget_AppCompat_ActionButton_Overflow = 2131427405;
+			
+			// aapt resource value: 0x7f0b018c
+			public const int SplashTheme = 2131427724;
 			
 			// aapt resource value: 0x7f0b00dd
 			public const int TextAppearance_AppCompat = 2131427549;
--- Mukes/Mukes.Android/Source/Activities/MenuListActivity.cs
+++ Mukes/Mukes.Android/Source/Activities/MenuListActivity.cs
@@ -8,7 +8,7 @@ using Android.Preferences;
 
 namespace Mukes.Droid
 {
-    [Activity(Label = "Mukes", Icon = "@drawable/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "Mukes", Icon = "@drawable/icon", Theme = "@style/MainTheme", MainLauncher = false, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MenuListActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         ListView _menuList;
